42e99fedbcde96eddaf3b998d0718060ab60eb8a
muneer464 <muneer464@gmail.com>
2018-02-11 11:09:14 +0200

    DigitalClock:

--- time_thread/DigitalClock.java
+++ time_thread/DigitalClock.java
@@ -1,5 +1,10 @@
 package time_thread;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,7 +20,15 @@ public class DigitalClock extends JFrame {
 		panel.add(label);
 		add(panel);		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(200, 200);
+		setSize(1400, 600);
+		label.setFont(new Font("ARIAL", Font.BOLD, 80));
+		label.setBackground(Color.black);
+		label.setForeground(Color.white);
+		label.setOpaque(true);
+		label.setPreferredSize(new Dimension(1200, 400 ));
+		label.setVerticalTextPosition(JLabel.CENTER);
+		label.setHorizontalTextPosition(JLabel.CENTER);;
+		label.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 3));
 		ct = new ClockThread(this);
 		setVisible(true);
 		setLocationRelativeTo(null);
